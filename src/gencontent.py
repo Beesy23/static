6c024bf638fa748f36c83d2889758ffbb28cf18e
Beesy23 <Nathan.Beese@gmail.com>
2024-09-13 20:59:42 +0100

    Added comments for main.py and gencontent.py

--- src/gencontent.py
+++ src/gencontent.py
@@ -10,14 +10,21 @@ def extract_title(markdown):
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    # Store from_path file to a variable
     with open(from_path) as content_file:
         contents_from = content_file.read()
+    # Store template_path file to a variable
     with open(template_path) as template_file:
         contents_template = template_file.read()
+    # Extract title from contents
     title = extract_title(contents_from)
+    # Convert contents to html string
     html_string = markdown_to_html_node(contents_from).to_html()
+    # Replace title and content placeholders in template with actual title and placeholders
     contents_template = contents_template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    # Creates dest_path directory if it does not already exist
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    # Write template to a new file
     with open(dest_path,"w") as html_file:
         html_file.write(contents_template)
 
@@ -25,22 +32,30 @@ def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
     print(f"Generating pages from {dir_path_content} to {dest_dir_path} using {template_path}")
     if not os.path.exists(dir_path_content): 
         raise FileNotFoundError(f"Error: Source directory '{dir_path_content}' does not exist")
+    # Iterates over files in dir_path_content directory
     for file_path in os.listdir(dir_path_content):
-            if file_path != "":
-                try:
-                    src_file_path = os.path.join(dir_path_content, file_path)
-                    dst_file_path = os.path.join(dest_dir_path, file_path)
-                    if os.path.isfile(src_file_path) and src_file_path.endswith(".md"):
-                        dst_file_path = dst_file_path.replace(".md",".html")
-                        generate_page(src_file_path, template_path, dst_file_path)
-                        print(f"Page from {src_file_path} generated to {dst_file_path}")
-                    elif os.path.isdir(src_file_path):
-                        os.makedirs(dst_file_path, exist_ok=True)
-                        generate_page_recursive(src_file_path, template_path, dst_file_path)
-                        print(f"Directory {src_file_path} generated to {dst_file_path}")
-                    else:
-                        print(f"{file_path} is not a markdown file or directory")
-                        continue
+            try:
+                # Create source file path and destination file path
+                src_file_path = os.path.join(dir_path_content, file_path)
+                dst_file_path = os.path.join(dest_dir_path, file_path)
+                # Checks if file path is a file that ends in .md (markdown file)
+                if os.path.isfile(src_file_path) and src_file_path.endswith(".md"):
+                    # Replaces .md extension with .html
+                    dst_file_path = dst_file_path.replace(".md",".html")
+                    # Generates html file
+                    generate_page(src_file_path, template_path, dst_file_path)
+                    print(f"Page from {src_file_path} generated to {dst_file_path}")
+                # Checks if file path is a directory
+                elif os.path.isdir(src_file_path):
+                    # Creates directory in destination directory if it doesn't already exist
+                    os.makedirs(dst_file_path, exist_ok=True)
+                    # Recursive to crawl over files in directory and generate .html files for them
+                    generate_page_recursive(src_file_path, template_path, dst_file_path)
+                    print(f"Directory {src_file_path} generated to {dst_file_path}")
+                # Deals with other file types
+                else:
+                    print(f"{file_path} is not a markdown file or directory")
+                    continue
 
-                except Exception as e:
-                    print(f"Error: {str(e)}")
+            except Exception as e:
+                print(f"Error: {str(e)}")
--- src/main.py
+++ src/main.py
@@ -8,35 +8,45 @@ def main():
     src_dir = "static"
     dst_dir = "public"
     try:
-        copy_static(src_dir, dst_dir)
+        # Copies everything in static directory to public directory
+        copy_dir(src_dir, dst_dir)
         print("Static files copied sucessfully")
     except Exception as e:
         print(f"Error occurred copying static files: {str(e)}")
 
     try:
+        # Generates .html files in public directory for all .md files in content directory 
         generate_page_recursive("content", "template.html", "public")
-        print("Function: generate_page_recursive completed")
+        print("Function: generate_page_recursive executed")
     except Exception as e:
         print(f"Error occured generating pages: {str(e)}")
 
-def copy_static(src, dst):
+def copy_dir(src, dst):
     try:
+        # Checks is destination directory already exists
         if os.path.exists(dst):
+            # Deletes destination directory for clean copy
             shutil.rmtree(dst)
+        # Creates new destination directory
         os.mkdir(dst)  
+        # Checks if source directory exists and raise an error if not
         if not os.path.exists(src): 
             raise FileNotFoundError(f"Error: Source directory '{src}' does not exist")
-        
+        # Iterates over file in source directory
         for item in os.listdir(src):
+            # Assigns file paths for files in source directory and destination directory
             s = os.path.join(src, item)
             d = os.path.join(dst, item)
             try:
+                # Checks if source directory file path is a file
                 if os.path.isfile(s):
+                    # Copies file in source directory to destination directory
                     shutil.copy(s, d)
                     print(f"Copied file {s} to {d}")
                 else:
-                    print(f"Creating diectory {d}")
-                    copy_static(s,d)
+                    # Creates source directory in destination directory
+                    print(f"Creating directory {d}")
+                    copy_dir(s,d)
             except Exception as e:
                 print(f"Error processing {s}: {str(e)}")
     except Exception as e:
